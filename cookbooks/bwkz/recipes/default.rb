d211798697745cc0e314b46f354b84a477f5f395
unknown <Administrator@WIN-JU7T1RIEGRF.bwkz.local>
2015-06-26 23:04:14 +0000

    updating the features;

--- cookbooks/bwkz/recipes/default.rb
+++ cookbooks/bwkz/recipes/default.rb
@@ -1,10 +1,29 @@
 
-# Install Roles Features
-%w[IIS-WebServerRole IIS-WebServer AppServer].each do |feature|
+
+# Install Supporting Services
+%w[WAS-WindowsActivationService WAS-NetFxEnvironment WAS-ConfigurationAPI IIS-LoggingLibraries IIS-HttpTracing IIS-HttpCompressionDynamic IIS-ManagementScriptingTools IIS-IIS6ManagementCompatibility IIS-BasicAuthentication IIS-ManagementService IIS-FTPSvc IIS-FTPSvc IIS-FTPExtensibility WCF-HTTP-Activation IIS-WindowsAuthentication IIS-DigestAuthentication IIS-ClientCertificateMappingAuthentication  IIS-IISCertificateMappingAuthentication IIS-URLAuthorization IIS-IPSecurity].each do |feature|
   windows_feature feature do
     action :install
   end
 end
+
+# Install Roles Features
+%w[IIS-WebServerRole IIS-WebServer NetFx3].each do |feature|
+  windows_feature feature do
+    action :install
+    all true
+  end
+end
+
+# Install Roles Features
+%w[ServicesForNFS-ServerAndClient ServerForNFS-Infrastructure ClientForNFS-Infrastructure NFS-Administration CoreFileServer].each do |feature|
+  windows_feature feature do
+    action :install
+    all true
+  end
+end
+ 
+
 
 # Install IIS Features
 %w[IIS-ApplicationDevelopment IIS-NetFxExtensibility IIS-ISAPIExtensions IIS-ISAPIFilter IIS-ASPNET IIS-HttpRedirect IIS-ASP IIS-HealthAndDiagnostics IIS-Security IIS-WebServerManagementTools IIS-FTPServer IIS-Performance].each do |feature|
