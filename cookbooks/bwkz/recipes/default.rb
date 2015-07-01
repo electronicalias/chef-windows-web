bdd71403977803e4b3cacfbe214fd2ae0a727bef
electronicalias <electronicalias@gmail.com>
2015-07-01 14:05:07 +0100

    Update default.rb
    ataewrwe

--- cookbooks/bwkz/recipes/default.rb
+++ cookbooks/bwkz/recipes/default.rb
@@ -59,7 +59,7 @@ end
 
 # do the same but map to testfu.chef.io domain
 if node['bwkzrole'] == "webserver"
-  %w[awstest1].each do |bwkzsite|
+  %w[awstest1 awstest2 awstest3].each do |bwkzsite|
     iis_pool bwkzsite do
       runtime_version "2.0"
       pipeline_mode :Integrated
